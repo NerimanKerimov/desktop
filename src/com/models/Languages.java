77280bee6d3ff91673f524d515cc7394aa3c0fb7
Marvin <marvin@evilinsult.com>
2016-10-31 12:43:03 +0100

    Initial Commit

---
+++ src/com/models/Languages.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.models;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Niyaz-laptop
+ */
+public class Languages implements Serializable{
+    
+    private int id;
+    private String lang;
+    private String fullLanguage;
+
+    public Languages() {
+    }
+
+   
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    public String getFullLanguage() {
+        return fullLanguage;
+    }
+
+    public void setFullLanguage(String fullLanguage) {
+        this.fullLanguage = fullLanguage;
+    }
+
+    @Override
+    public String toString() {
+        return  fullLanguage ;
+    }
+
+    
+    
+    
+    
+    
+}
